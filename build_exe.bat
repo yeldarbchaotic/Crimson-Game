0de2ebe4e6b4b5bfca72ade5d3d713e8623d301e
yeldarbchaotic <yeldarbchaotic@yahoo.com>
2015-01-03 16:26:51 -0500

    Create

---
+++ build_exe.bat
@@ -0,0 +1,8 @@
+python setup.py py2exe
+pause
+
+rem TO DO:
+rem  - Delete old lib folder
+rem  - Delete build folder
+rem  - Move all files from dist folder to parent dir
+rem  - Delete dist folder
